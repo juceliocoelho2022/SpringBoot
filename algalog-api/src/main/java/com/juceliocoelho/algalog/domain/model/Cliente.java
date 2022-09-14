ef685dcb55e373fb8741f165dd2408a70f3e4158
Jucelio Farias Coelho <juceliocoelho2022@gmail.com>
2022-09-14 10:23:32 -0300

    Usando_Jakarta

--- algalog-api/src/main/java/com/juceliocoelho/algalog/domain/model/Cliente.java
+++ algalog-api/src/main/java/com/juceliocoelho/algalog/domain/model/Cliente.java
@@ -1,13 +1,22 @@
 package com.juceliocoelho.algalog.domain.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@Entity
+@Table
 public class Cliente {
 
-	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String nome;
 	private String email;
